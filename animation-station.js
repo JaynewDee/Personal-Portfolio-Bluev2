7129d87a6900459fe700e476d37df2d0b8397893
NuminousBlue <jdiehl2236@gmail.com>
2021-10-16 07:53:53 -0500

    viewport optimization

--- animation-station.js
+++ animation-station.js
@@ -2,18 +2,25 @@ const properNoun = document.getElementById('nametag');
 
 window.addEventListener('load', (event) => {
      setInterval(frame, 13);
-     let pos = 0;
+
+     let position = 0;
      let speed = 0;
      let fade = 0.0;
+     let clear = null;
+     let stopper = properNoun.style.opacity;
+     console.log(stopper)
      function frame() {
-          
-          if (pos < 180) {
-               pos++;
+          if (position < 120) {
+               position++;
                $('#nametag').css({'margin-left': `${speed}px`, 'opacity': `${fade}`});
-               fade += 1/200;
-               speed++;
-          } else {
-               return;
-          }
+               fade += 1/100;
+               speed++;}  
+
+
+               // else if (stopper == 1) {
+          //      properNoun.style.margin = auto;
+          //      // clearInterval(clear);
+          //      return;
+          // }
      }
 })
